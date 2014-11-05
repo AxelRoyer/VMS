af3dc702f2f5f3220241ed466e27ccefa795905d
Axel Royer <axel.royer@caplin.com>
2014-11-05 20:38:32 +0000

    added empty functions in MainMenu

--- components/main-menu.js
+++ components/main-menu.js
@@ -8,6 +8,18 @@ MainMenuProto.attachedCallback = function() {
     console.log("main menu attached callback");
 };
 
+MainMenuProto.addItem = function(label, link, position) {
+
+};
+
+MainMenuProto.addItems = function(items) {
+
+};
+
+MainMenuProto.removeItem = function(label) {
+
+};
+
 MainMenu = document.registerElement('main-menu', {
     prototype: MainMenuProto
 });
