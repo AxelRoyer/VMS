28fc9ae65f37ac28455d490f8f0ccabf4fa1e603
Axel Royer <axel.royer@caplin.com>
2014-12-04 20:40:29 +0000

    added jsdom for testing

--- tests/main-menu-tests.js
+++ tests/main-menu-tests.js
@@ -1,2 +1,27 @@
-var MainMenu = require("mainMenu");
-console.log("mainmenu", MainMenu);
+var jsdom = require("jsdom");
+var window = jsdom.jsdom().parentWindow;
+
+require("./../libs/webcomponents-lite.min");
+require("./../sources/bundle");
+var MainMenu = require("./../sources/main-Menu");
+
+var mainMenu = null;
+
+describe("MainMenu", function() {
+
+    describe("after being attached", function() {
+        it("should init tab as an empty array", function() {
+            mainMenu = document.createElement("main-menu");
+            document.appendChild(mainMenu);
+            assert.equal(true, true);
+        });
+    });
+
+    describe("addItem function", function() {
+        it("should add an item into the array of items", function() {
+            mainMenu = document.createElement("main-menu");
+            document.appendChild(mainMenu);
+            assert.equal(true, true);
+        });
+    });
+});
